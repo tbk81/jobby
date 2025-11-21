8d43ae778715e032fed51d0c65d68f12b3dcd275
MBP <dev@tbk81.com>
2025-11-21 14:22:25 -0800

    jobby

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import requests
-from bs4 import BeautifulSoup
+import os
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
 
 job_url = "https://recruiting.paylocity.com/Recruiting/Jobs/All/d1d20c2d-0e1d-4869-820d-a5b454cfba0b/ANAPTYSBIO-INC"
+
+# Chrome and driver setup
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+user_data_dir = os.path.join(os.getcwd(), "chrome_profile")
+chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get(endpoint)
+
 
 
 def write_site(site):
@@ -15,6 +30,7 @@ def write_site(site):
 with open('website.html') as f:
     data = f.read()
 soup = BeautifulSoup(data, 'html.parser')
-job_titles = soup.find_all()
+job_titles = soup.find_all('div')  #, class_='job-listing-container')
+print(job_titles)
 
 
