d220a1a1214c8472f233aad92dac602083813898
MBP <dev@tbk81.com>
2026-01-21 14:27:28 -0800

    jobby

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import requests
 import os
+import requests
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
