0257097dcaad459c4591796ca670d952dd2c9eda
tbk81 <dev@tbk81.com>
2026-01-27 05:05:24 -0800

    jobby

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from company_manager import *
+from site_scraper import *
+from site_scraper import write_site
 
 # new_company = ["Erasca", "https://www.erasca.com/open-positions/"]
 
 # add_company(new_company)
 # mira = url_grabber("Mirador")
 # print(mira)
+
+site_data = write_site(url_grabber("Erasca"), "erasca")
+
--- site_scraper.py
+++ site_scraper.py
@@ -6,6 +6,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
 
+headers = {
+    "USER-AGENT": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
+                  "(KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",
+
+    }
 
 # Chrome and driver setup for selenium
 def chrome_driver(url):
@@ -19,9 +24,9 @@ def chrome_driver(url):
     driver.get(url)
 
 # Pulls site HTML data to test parsing; this does not work for dynamic sites
-def write_site(site):
-    job_response = requests.get(site)
-    with open('html_data/website.html', 'w') as file:
+def write_site(site, name):
+    job_response = requests.get(site, headers=headers)
+    with open(f'html_data/{name}.html', 'w') as file:
         file.write(job_response.text)
 
 
@@ -32,3 +37,4 @@ def title_parser():
     soup = BeautifulSoup(data, 'html.parser')
     job_titles = soup.find_all('div')  #, class_='job-listing-container')
     print(job_titles)
+
