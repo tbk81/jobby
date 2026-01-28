48c9f3c9eb2c73b8fff60a72e40298b103c265ac
tbk81 <dev@tbk81.com>
2026-01-28 05:18:13 -0800

    jobby

--- job_parsers/erasca.py
+++ job_parsers/erasca.py
@@ -1,26 +1,12 @@
 from bs4 import BeautifulSoup
 
-# for testing how to parse the job titles and descriptions
-def title_parser():
-    with open('html_data/website.html') as f:
-        data = f.read()
-    soup = BeautifulSoup(data, 'html.parser')
-    job_titles = soup.find_all('div')
-    print(job_titles)
-
-
-with open('/Users/trevor/jobby/html_data/erasca.html') as file:
+# with open('/Users/trevor/jobby/html_data/erasca.html') as file:
+with open('/home/trevor/python-projects/jobby/html_data/erasca.html') as file:
     erasca_data = file.read()
-test_soup = BeautifulSoup(erasca_data, 'html.parser')
-# print(test_soup.find_all(class_='title-contaier'))
-# print(test_soup.find_all(name='span'))
-spans = test_soup.find_all(name='span')
-count = 0
-for s in spans:
-    count += 1
-    print(f'{s}\t{s.text}\t{count}')
-for s in spans:
-    if "data-v-638fa1cc" in s:
-        print(s.text)
+soup = BeautifulSoup(erasca_data, 'html.parser')
 
-
+job_titles = soup.find_all('div', class_='title-contaier')
+job_locations = soup.find_all('div', class_='job-location')
+job_urls = soup.find_all('div', class_='job-link')
+for title in job_titles:
+    print(title.text.strip())
