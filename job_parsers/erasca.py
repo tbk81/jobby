ee5196a8638d80bed623ef04c46bda9bf1093f8d
tbk81 <dev@tbk81.com>
2026-01-28 06:01:39 -0800

    jobby

--- job_parsers/erasca.py
+++ job_parsers/erasca.py
@@ -6,7 +6,13 @@ with open('/home/trevor/python-projects/jobby/html_data/erasca.html') as file:
 soup = BeautifulSoup(erasca_data, 'html.parser')
 
 job_titles = soup.find_all('div', class_='title-contaier')
-job_locations = soup.find_all('div', class_='job-location')
-job_urls = soup.find_all('div', class_='job-link')
-for title in job_titles:
-    print(title.text.strip())
+job_locations = soup.find_all('div', class_='info-container')
+job_urls = soup.find_all('a', class_='job-link')
+
+
+for i in range(len(job_titles)):
+    print(job_titles[i].text.strip())
+    print(job_locations[i].get_text(strip=True))
+    print(job_urls[i]['href'].strip())
+
+# text = element.get_text(strip=True).replace('→', '')
