0957c07e57c9b1346fb1a6ee97c48ad0a43936a4
MBP <dev@tbk81.com>
2026-02-04 14:57:01 -0800

    jobby

--- db_manager.py
+++ db_manager.py
@@ -1,18 +1,26 @@
 import sqlite3
 from sqlalchemy import Integer, String, Float
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+import datetime
 
 companies_db = sqlite3.connect('databases/companies.db')
 companies_db_cursor = companies_db.cursor()
 
-companies_db_cursor.execute("CREATE TABLE companies (id INTEGER PRIMARY KEY, company varchar(250) "
-                            "NOT NULL UNIQUE, url varchar(250) NOT NULL, date )")
+# companies_db_cursor.execute("CREATE TABLE companies (id INTEGER PRIMARY KEY, company varchar(250) "
+#                             "NOT NULL UNIQUE, url varchar(250) NOT NULL, date )")
 
-class Book(db.Model):
+# new_column = "ALTER TABLE companies ADD COLUMN addDate TIMESTAMP"
+# companies_db_cursor.execute(new_column)
+
+class Company:
     company: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
     url: Mapped[str] = mapped_column(String(250), nullable=False)
-    date: Mapped[float] = mapped_column(Float, nullable=False)
+    date: Mapped[str] = mapped_column(String(250), nullable=False)
 
-    # This will allow each book object to be identified by its title when printed.
-    def __repr__(self):
-        return f'<Book {self.title}>'
+def add(name, url):
+    new_company = Company(
+        company = name,
+        url = url,
+        date =
+    )
+
