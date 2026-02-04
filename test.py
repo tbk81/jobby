d463a223df7b1c36b66b618643d4b341fc4813da
MBP <dev@tbk81.com>
2026-02-03 16:02:24 -0800

    jobby

--- test.py
+++ test.py
@@ -7,19 +7,19 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
 
-site_driver = sel_driver(url_grabber("Mirador"))
-
-try:
-    button = site_driver.find_element(By.ID, "CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll")
-except NoSuchElementException:
-    print("Accept cookies button not found")
-else:
-    button.click()
+# site_driver = sel_driver(url_grabber("Mirador"))
 
 # try:
-frame = site_driver.find_element(By.ID, "grnhse_iframe")
-site_driver.switch_to.frame(frame)
-html = site_driver.page_source
+#     button = site_driver.find_element(By.ID, "CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll")
+# except NoSuchElementException:
+#     print("Accept cookies button not found")
+# else:
+#     button.click()
+
+# try:
+# frame = site_driver.find_element(By.ID, "grnhse_iframe")
+# site_driver.switch_to.frame(frame)
+# html = site_driver.page_source
     # job_titles = site_driver.find_elements(By.XPATH, "//p[@class='body body--medium']")
     # job_urls = site_driver.find_elements(By.TAG_NAME, "a")
 # except NoSuchElementException:
@@ -28,9 +28,9 @@ html = site_driver.page_source
 #     for i in range(len(job_titles))[1:]:
 #         print(job_titles[i].text)
 #         # print(job_urls[i].text)
-site_driver.quit()
-with open(f'html_data/mirador.html', 'w') as file:
-    file.write(html)
+# site_driver.quit()
+# with open(f'html_data/mirador.html', 'w') as file:
+#     file.write(html)
 # print(html)
 
 
@@ -39,13 +39,13 @@ with open('/Users/trevor/jobby/html_data/mirador.html') as file:
 # with open('/home/trevor/python-projects/jobby/html_data/mirador.html') as file:
     mirador_data = file.read()
 soup = BeautifulSoup(mirador_data, 'html.parser')
-
-job_titles = soup.find_all('p', class_='body.body--medium')
+#
+job_titles = soup.find_all('p', class_='body body--medium')
 # job_locations = soup.find_all('span', class_='sort-by-location posting-category small-category-label location')
-# job_urls = soup.find_all('a', class_='posting-btn-submit template-btn-submit cerulean')
+job_urls = soup.find_all('a')
 
 for i in range(len(job_titles))[1:]:
     print(job_titles[i].text)
     # print(job_locations[i].get_text(strip=True))
-    # print(job_urls[i]['href'].strip())
+    print(job_urls[i-1]['href'].strip())
     # print("\n")
