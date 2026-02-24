b7c8a06886c646bd04af4448313337b21dffcf61
MBP <dev@tbk81.com>
2026-02-24 15:59:51 -0800

    jobby

--- src/job_parsers/erasca.py
+++ src/job_parsers/erasca.py
@@ -1,22 +1,26 @@
 from bs4 import BeautifulSoup
-from src.db_manager import add_job
+from src.site_scraper import write_site
 
-with open('src/html_data/erasca.html') as file:
-    erasca_data = file.read()
-soup = BeautifulSoup(erasca_data, 'html.parser')
+def scrape_jobs(url):
+    write_site(url, "erasca")
+    scraped_data = []
+    with open('src/html_data/erasca.html') as file:
+        erasca_data = file.read()
+    soup = BeautifulSoup(erasca_data, 'html.parser')
 
-job_titles = soup.find_all('div', class_='title-contaier')
-job_locations = soup.find_all('div', class_='info-container')
-job_urls = soup.find_all('a', class_='job-link')
+    job_titles = soup.find_all('div', class_='title-contaier')
+    job_locations = soup.find_all('div', class_='info-container')
+    job_urls = soup.find_all('a', class_='job-link')
 
+    for i in range(len(job_titles)):
+        title = job_titles[i].text.strip()
+        location = job_locations[i].get_text(strip=True).replace('→', '')
+        job_url = job_urls[i]['href'].strip()
 
-# for i in range(len(job_titles)):
-#     print(job_titles[i].text.strip())
-#     print(job_locations[i].get_text(strip=True).replace('→', ''))
-#     print(job_urls[i]['href'].strip())
+        scraped_data.append({
+            "title": title,
+            "location": location,
+            "url": job_url
+        })
 
-for i in range(len(job_titles)):
-    title = job_titles[i].text.strip()
-    local = job_locations[i].get_text(strip=True).replace('→', '')
-    url = job_urls[i]['href'].strip()
-    add_job("Erasca", title, local, url)
+    return scraped_data
--- src/job_parsers/janux.py
+++ src/job_parsers/janux.py
@@ -1,16 +1,27 @@
 from bs4 import BeautifulSoup
-from src.db_manager import add_job
+from src.site_scraper import write_site
 
-with open('src/html_data/janux.html') as file:
-    janux_data = file.read()
-soup = BeautifulSoup(janux_data, 'html.parser')
 
-job_titles = soup.find_all('h5')
-job_locations = soup.find_all('span', class_='sort-by-location posting-category small-category-label location')
-job_urls = soup.find_all('a', class_='posting-btn-submit template-btn-submit cerulean')
+def scrape_jobs(url):
+    write_site(url, "janux")
+    scraped_data = []
+    with open('src/html_data/janux.html') as file:
+        janux_data = file.read()
+    soup = BeautifulSoup(janux_data, 'html.parser')
 
-for i in range(len(job_titles)):
-    print(job_titles[i].text.strip())
-    print(job_locations[i].get_text(strip=True))
-    print(job_urls[i]['href'].strip())
-    print("\n")
+    job_titles = soup.find_all('h5')
+    job_locations = soup.find_all('span', class_='sort-by-location posting-category small-category-label location')
+    job_urls = soup.find_all('a', class_='posting-btn-submit template-btn-submit cerulean')
+
+    for i in range(len(job_titles)):
+        title = job_titles[i].text.strip()
+        location = job_locations[i].get_text(strip=True)
+        job_url = job_urls[i]['href'].strip()
+
+        scraped_data.append({
+            "title": title,
+            "location": location,
+            "url": job_url
+        })
+
+    return scraped_data
