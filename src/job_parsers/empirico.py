0519689f7e44170532ee0999a4f542f21af60ec3
MBP <dev@tbk81.com>
2026-02-24 15:09:16 -0800

    jobby

--- src/job_parsers/empirico.py
+++ src/job_parsers/empirico.py
@@ -1,14 +1,26 @@
 from bs4 import BeautifulSoup
+from src.site_scraper import write_site
 
-with open('src/html_data/empirico.html') as file:
-    empirico_data = file.read()
-soup = BeautifulSoup(empirico_data, 'html.parser')
+def scrape_jobs(url):
+    write_site(url, "empirico")
+    scraped_data = []
+    with open('src/html_data/empirico.html') as file:
+        empirico_data = file.read()
+    soup = BeautifulSoup(empirico_data, 'html.parser')
 
-job_titles = soup.find_all('h3')
-job_locations = soup.find_all('div', class_='location')
+    job_titles = soup.find_all('h3')
+    job_locations = soup.find_all('div', class_='location')
 
-for i in range(len(job_titles)-1):
-    print(job_titles[i].text.strip())
-    print(job_locations[i].get_text(strip=True))
-    print("https://www.empiri.co/careers/")
-    print("\n")
+    for i in range(len(job_titles)-1):
+        title = job_titles[i].text.strip()
+        location = job_locations[i].get_text(strip=True)
+        job_url = "https://www.empiri.co/careers/"
+        print("\n")
+
+        scraped_data.append({
+            "title": title,
+            "location": location,
+            "url": job_url
+        })
+
+    return scraped_data
